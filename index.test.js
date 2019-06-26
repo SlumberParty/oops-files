a613fbe174026c0094e48139c401bcad63c1a424
Claire Cassidy <clacassidy@gmail.com>
2019-06-26 07:34:24 -0700

    started trying to write tests and working out what to do in the readme

--- index.test.js
+++ index.test.js
@@ -1,4 +1,13 @@
+const fs = require('fs');
+const { join } = require('path');
+
 
 describe('rename files', () => {
-  beforeEach 
-})
+    beforeEach(done => {
+        fs.writeFile(join(__dirname, '1.txt'), 'whyy', done));
+    })
+
+  afterEach(done => {
+    fs.unlink(join(__dirname, '1.txt'), done);
+  })
+});
